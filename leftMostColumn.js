802346bddaf8cbb915fd74208f455abf78a9fc86
savely <avelikodny@gmail.com>
2020-04-21 15:05:12 +0300

    not working yet

--- leftMostColumn.js
+++ leftMostColumn.js
@@ -20,5 +20,52 @@
  * @return {number}
  */
 var leftMostColumnWithOne = function(binaryMatrix) {
-    
+
+    const [rows, cols] = binaryMatrix.dimensions()
+
+    let i = 0, j = cols-1
+
+    while(i < rows-1 && j >= 0) {
+        if(binaryMatrix.get(i,j) == 0) {
+            i++
+        } else {
+            j--
+        }
+    }
+
+    return (j === cols-1) ? -1 : j + 1
 };
+
+const first = function(arr, start, end) {
+
+    if(start > end) return -1
+
+    if(start === end) return arr[end] === 1 ? end : -1
+    let lo = start
+    let hi = end
+
+    while (hi > lo) {
+        let mid = Math.floor((hi+lo) / 2)
+
+        if(arr[mid] === 1 && mid === lo) return lo 
+
+        if(arr[mid] === 1 && arr[mid-1] === 0) return mid
+
+        if(arr[mid] === 0) {
+           lo = mid+1
+        } else {
+            hi = mid
+        }
+    }
+
+    if(hi === lo) return arr[lo] === 1 ? lo : -1
+    if(hi - lo === 1) {
+        return arr[hi] === 1 && arr[lo] === 0 ? hi : -1
+       }
+       return -1
+
+}
+
+const f = leftMostColumnWithOne()
+
+console.log(f([1,0,1,1],1,2))
