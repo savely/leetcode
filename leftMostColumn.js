d32197dc46f2e88dd676a4f18ed598aa26f7e491
savely <avelikodny@gmail.com>
2020-04-21 12:16:01 +0300

    initial lmc

---
+++ leftMostColumn.js
@@ -0,0 +1,24 @@
+/**
+ * // This is the BinaryMatrix's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * function BinaryMatrix() {
+ *     @param {integer} x, y
+ *     @return {integer}
+ *     this.get = function(x, y) {
+ *         ...
+ *     };
+ *
+ *     @return {[integer, integer]}
+ *     this.dimensions = function() {
+ *         ...
+ *     };
+ * };
+ */
+
+/**
+ * @param {BinaryMatrix} binaryMatrix
+ * @return {number}
+ */
+var leftMostColumnWithOne = function(binaryMatrix) {
+    
+};
