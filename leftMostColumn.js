73d3bda3869b8c23ee91414c1a075f73121d7dac
savely <avelikodny@gmail.com>
2020-04-21 15:23:10 +0300

    removed binary search, cleaned

--- leftMostColumn.js
+++ leftMostColumn.js
@@ -35,37 +35,3 @@ var leftMostColumnWithOne = function(binaryMatrix) {
 
     return (j === cols-1) ? -1 : j + 1
 };
-
-const first = function(arr, start, end) {
-
-    if(start > end) return -1
-
-    if(start === end) return arr[end] === 1 ? end : -1
-    let lo = start
-    let hi = end
-
-    while (hi > lo) {
-        let mid = Math.floor((hi+lo) / 2)
-
-        if(arr[mid] === 1 && mid === lo) return lo 
-
-        if(arr[mid] === 1 && arr[mid-1] === 0) return mid
-
-        if(arr[mid] === 0) {
-           lo = mid+1
-        } else {
-            hi = mid
-        }
-    }
-
-    if(hi === lo) return arr[lo] === 1 ? lo : -1
-    if(hi - lo === 1) {
-        return arr[hi] === 1 && arr[lo] === 0 ? hi : -1
-       }
-       return -1
-
-}
-
-const f = leftMostColumnWithOne()
-
-console.log(f([1,0,1,1],1,2))
