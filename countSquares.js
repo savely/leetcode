f3338205052dfcf6a64af33c7bad24ccf8fb23cc
savely <avelikodny@gmail.com>
2020-05-21 11:46:48 +0300

    accepted

---
+++ countSquares.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number}
+ */
+var countSquares = function(matrix) {
+    
+    let count = 0
+
+    for(let i = 0; i < matrix.length; i++) {
+        for(let j = 0; j < matrix[0].length; j++) {
+            if(i > 0 && j > 0 && matrix[i][j] > 0) {
+               matrix[i][j] += Math.min(matrix[i-1][j], matrix[i][j-1],matrix[i-1][j-1])
+            }
+            count+= matrix[i][j]
+        }
+    }
+    return count
+};
