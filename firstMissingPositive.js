96fd5de1093709f5388fc17fcaed7e4e9ce56f66
savely <avelikodny@gmail.com>
2020-06-12 08:15:36 +0300

    remived debug info

--- firstMissingPositive.js
+++ firstMissingPositive.js
@@ -46,5 +46,5 @@ var firstMissingPositive = function(nums) {
 
 //console.log(firstMissingPositive([1,2,0]))
 //console.log(firstMissingPositive([3,4,-1,1]))
-console.log(firstMissingPositive([7,8,9,11,12]))
-console.log(firstMissingPositive([1,1,1,1,1]))
+//console.log(firstMissingPositive([7,8,9,11,12]))
+//console.log(firstMissingPositive([1,1,1,1,1]))
