5637b330054a7290bf28a0ed1c2743b288fa58ba
savely <avelikodny@gmail.com>
2021-04-23 00:07:18 +0300

    working but using too much memory to pass

--- expressionAddOperators.js
+++ expressionAddOperators.js
@@ -21,46 +21,55 @@ Output: ["1*2*3","1+2+3"]
     let dp = {};
     const ops = {'+' : (x, y) => x + y,
                  '-' : (x, y) => x - y,
-                 '*' : (x,y) => x * y}
+                 '*' : (x,y) => x * y,
+                 ''  : (x, y) => x === 0 ? NaN : 10 * x + y
+            }
 
-    const evaluate = (expr, hasMultiply = false) => {
-        
-        const stack = [expr[0]]
+     const evaluate = (expr) => {
 
-        let i = 1
+         const hasOp  = expr.includes('')
+         const hasMultiply = expr.includes('*')
+         const stack = [expr[0]]
 
-        while (i < expr.length) {
-         
-         const op = expr[i++]
-         const n  = expr[i++]
-         const top = stack.length - 1
+         let i = 1
 
-         if((op === '+' || op === '-') && hasMultiply) {
+         while (i < expr.length) {
+
+             const op = expr[i++]
+             const n  = expr[i++]
+             const top = stack.length - 1
+
+            if(op === '*' && hasOp) {
+               stack.push(op, n)
+               continue
+             }
+
+        if((op === '+' || op === '-') && (hasMultiply || hasOp)) {
             stack.push(op, n)
             continue
          }
-          
+
          stack[top] = (ops[op])(stack[top], n) 
-        }
-
-        if(stack.length === 1) return stack[0]
-
-        return evaluate(stack, false)
-
     }
+
+     if(stack.length === 1) return stack[0]
+
+    return evaluate(stack)
+    };
+
     dp[num[0]] = [ [nums[0]] ]
 
    for(let i = 1; i < nums.length; i++) {
        
-       const digit = nums[i], str = num[i], nextDp = {} 
-       
+       const digit = nums[i], nextDp = {} 
+
        for(const key in dp) {
            const expressions = dp[key]
 
            for (const expr of expressions) {
             for(const op in ops) {
 
-                const newExpr = [...expr, op, digit], nextKey = evaluate(newExpr, newExpr.includes('*'))
+                const newExpr = [...expr, op, digit], nextKey = evaluate(newExpr)
 
                 if(!nextDp[nextKey]) {
                    
@@ -72,12 +81,26 @@ Output: ["1*2*3","1+2+3"]
            }
        }
         dp = nextDp
+        delete nextDp
    }
+
+   const n = parseInt(num)
 
     return dp[target] ? dp[target].map(arr => arr.join('')) : []
 };
 
+let str = "123456789"
+let target = 5
 
-let str = "232", target = 8
+console.time('a')
+console.table(addOperators(str, target))
+console.timeEnd('a')
 
-console.log(addOperators(str, target))
+console.dir(process.memoryUsage())
+
+
+let expr = [3,'<',4,'*',5,'<',6,'-',2,'-',3,'+',7,'<',4,'<',9,'<',0]
+expr = [3,'+',4,'',5,'',6,'*',2,'',3,'+',7,'',4,'-',9,'-',0]
+expr = [3,'+',4,'',5,'',6,'*',2,'',3,'+',7,'',4,'-',9,'-',0]
+
+//console.log(evaluate(expr))
