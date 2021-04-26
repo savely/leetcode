a87c7a6f1c2cd1656d0bd26bf39f81b6a331c3d1
savely <avelikodny@gmail.com>
2021-04-26 20:11:30 +0300

    min heap version. accepted

--- furthestBuilding.js
+++ furthestBuilding.js
@@ -1,3 +1,93 @@
+/*
+You are given an integer array heights representing the heights of buildings, some bricks, and some ladders.
+
+You start your journey from building 0 and move to the next building by possibly using bricks or ladders.
+
+While moving from building i to building i+1 (0-indexed),
+
+If the current building's height is greater than or equal to the next building's height, you do not need a ladder or bricks.
+If the current building's height is less than the next building's height, you can either use one ladder or (h[i+1] - h[i]) bricks.
+Return the furthest building index (0-indexed) you can reach if you use the given ladders and bricks optimally.
+*/
+function Heap(compareFunc) {
+    this.arr = []
+    this.compareFunc = compareFunc
+}
+
+Heap.prototype.pop = function() {
+
+  if(this.size() === 0) return undefined
+  
+  const val = this.arr[0]
+
+  const last = this.arr.pop()
+
+  if(this.arr.length === 0) return val
+
+  this.arr[0] = last
+  this.sinkDown(0)
+
+  return val
+}
+
+Heap.prototype.push = function(val) {
+this.arr.push(val)
+this.bubbleUp(this.arr.length-1)
+}
+
+Heap.prototype.size = function() {
+    return this.arr.length
+}
+
+Heap.prototype.bubbleUp = function(n) {
+  const val = this.arr[n]
+
+  while(n > 0) {
+      const parentN = Math.floor((n+1)/2)-1
+      const parent = this.arr[parentN]
+
+      if(this.compareFunc(val,parent) > -1) break
+
+      this._swap(n, parentN)
+      n = parentN
+  }
+}
+
+Heap.prototype.sinkDown = function(n) {
+
+    const val = this.arr[n]
+    const len = this.arr.length
+
+    while(true) {
+       let child2N = (n+1) * 2
+       let child1N = child2N -1
+       let swap  = null
+       let child1 = null
+       let child2 = null
+
+       if(child1N < len) {
+         child1 = this.arr[child1N]
+          swap = this.compareFunc(val,child1) > -1 ? child1N : swap
+       }
+
+       if(child2N < len) {
+        child2 = this.arr[child2N]
+        swap = this.compareFunc(swap === null? val: child1, child2) > -1 ? child2N : swap
+     }
+
+     if(swap === null) break
+
+    this._swap(swap, n)
+    n = swap
+    }
+}
+
+Heap.prototype._swap = function(m,n) {
+    const tmp = this.arr[m]
+    this.arr[m] = this.arr[n]
+    this.arr[n] = tmp
+} 
+
 /**
  * @param {number[]} heights
  * @param {number} bricks
@@ -6,43 +96,48 @@
  */
  var furthestBuilding = function(heights, bricks, ladders) {
     
-    let dp = [[bricks,ladders]]
-    
-    for(let i = 1; i < heights.length; i++) {
-        
-        const prev = heights[i - 1], curr = heights[i]
-        
-        if(prev >= curr) continue;
-        
-        const newDp = [], diff = curr - prev, set = new Set()
-        
-        let hash
-        
-        for(const [bricks, ladders] of dp) {
-            
-            
-            if(bricks >= diff) {
-                hash = `${bricks - diff}|${ladders}`
-                if(!set.has(hash)) {
-                    set.add(hash)
-                    newDp.push([bricks - diff, ladders])    
-                }
-                   
-                
-            }
-            
-            if(ladders > 0) {
-              hash = `${bricks}|${ladders - 1}`
-              
-              if(!set.has(hash))
-                  set.add(hash)
-                   newDp.push([bricks, ladders - 1])
-            }
+    let i = 1
+
+    const heap = new Heap((x, y) => x - y)
+
+    while(ladders > 0 && i < heights.length) {
+
+       const prev = heights[i - 1], curr = heights[i], diff = curr - prev
+
+       if(diff <= 0) {
+           i++
+           continue
+       }
+
+       heap.push(diff)
+
+       ladders --
+       i++
+    }
+
+    while(bricks > 0 && i < heights.length) {
+
+        const prev = heights[i - 1], curr = heights[i], diff = curr - prev
+
+        if(diff <= 0)  {
+            i++
+            continue
         }
         
-        if(!newDp.length) return i - 1
-        dp = newDp
+        heap.push(diff)
+
+        const min = heap.pop()
+
+        if(bricks < min) return i - 1;
+
+        bricks -= min
+
+        i++
     }
-    
-    return heights.length - 1
+
+    while(i < heights.length && heights[i - 1] >= heights[i]) {
+        i++
+    }
+   
+    return i - 1 
 };
