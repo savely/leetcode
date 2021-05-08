e6e801b3770539c361522c6828d547ba32aba826
savely <avelikodny@gmail.com>
2021-05-08 17:18:36 +0300

    accepted

--- superPalindromes.js
+++ superPalindromes.js
@@ -34,84 +34,40 @@ left is less than or equal to right.
  * @return {number}
  */
  var superpalindromesInRange = function(left, right) {
-    
-     const palindromes = (l,r) => {
 
-        const prev = [], set = new Set()
-   
-        for(let i = 1; i <= 9; i++) {
-            prev.push(`${i}`)
-   
-            if(i < 4 && i >= l && i < r) {
-                set.add(BigInt(i))
-            }
+    const palindromesFor = (n) => {
+      
+       if(n < 10) return [BigInt(`${n}${n}`), BigInt(n)]
+
+       const str = n.toString(), rev = [...str].reverse().join('')
+
+       return [BigInt(`${str}${rev}`), BigInt(`${str}${rev.slice(1)}`)]
+
+    }
+
+    const isPalindrome = (n) => {
+
+        const str = n.toString()
+
+        return str === [...str].reverse().join('')
+    }
+
+    let ans = 0
+
+    for(let i = 1; i < 1e5; i++) {
+
+        const ps = palindromesFor(i)
+
+        if(ps[1] * ps[1] > right) return ans
+
+        for(p of ps) {
+            const sq = p * p
+            ans += (sq >= left && sq <= right && isPalindrome(sq)) ? 1 : 0 
         }
-   
-       
-       const inserts = ['0','1','2','3','4','5','6','7','8','9','00']
-       const digits  = ['1','2','3','4','5','6','7','8','9']
-   
-        while(prev.length) {
-   
-           const next  = new Set() 
-   
-           while (prev.length){
-             
-               const str = prev.pop()
+    }
 
-               for(const dig of digits) {
-                   
-                const  num = dig.concat(str).concat(dig)
-                const bigNum = BigInt(num) 
-
-                    if(bigNum <= r && !set.has(bigNum)) {
-                        next.add(num)
-                        if(bigNum >= l) set.add(bigNum)
-                    }
-                }               
-
-   
-               if(str.length % 2) {
-                   const even = str.concat([...str].reverse().join(''))
-                   const bigEven = BigInt(even)
-   
-                   if(bigEven <= r && !set.has(bigEven)) {
-                       next.add(even)
-                       if(bigEven >= l) set.add(bigEven)
-                   }
-                  continue 
-               }
-               
-               const half = str.length / 2, fst = str.slice(0, half), last = str.slice(half, str.length + 1)
-               for(const num of inserts) {
-                   
-                   const odd = fst.concat(num).concat(last)
-                   const bigOdd = BigInt(odd) 
-
-                   if(bigOdd <= r && !set.has(bigOdd)) {
-                       next.add(odd)
-                       if(bigOdd >= l) set.add(bigOdd)
-                   }
-               }
-           }
-   
-           prev.push(...next)
-       }
-     return [...set]
-   }
-
-    const palindromesArr = palindromes(Math.floor(Math.sqrt(parseInt(left))), Math.ceil(Math.sqrt(parseInt(right))))    
-
-    //console.table(palindromesArr)    
-
-    return palindromesArr.filter(bigN => {
-        const arr = [...(bigN * bigN).toString()], isSuper = arr.join('') === arr.reverse().join('')
-
-       if(isSuper) console.log([bigN, bigN * bigN])
-        return isSuper
-    }).length
-};
-
+    return ans
+ }
 
 let start = "40000000000000000"
 end = "50000000000000000"
@@ -119,4 +75,6 @@ end = "50000000000000000"
 start  = "99999999999999"
 end = "999999999999999999"
 
+//start = "4"
+//end   = "1000" 
 console.log(superpalindromesInRange(start, end))
