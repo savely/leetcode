d7df31a75d3b200f49291894aac2644829623274
savely <avelikodny@gmail.com>
2021-05-10 18:29:11 +0300

    accepted word search

--- wordSearch2.js
+++ wordSearch2.js
@@ -1,3 +1,10 @@
+/*
+#212. Word Search II
+Given an m x n board of characters and a list of strings words, return all words on the board.
+
+Each word must be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. 
+The same letter cell may not be used more than once in a word.
+*/
 var Trie = function() {
     this._hash = new Map()
     this._symbol = ''
