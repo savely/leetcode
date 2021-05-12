3047a7d49c67e672892f280b3608bc0d5874a53b
savely <avelikodny@gmail.com>
2021-05-12 20:21:15 +0300

    memoiZation, not accepted 65/94

--- minJumps.js
+++ minJumps.js
@@ -20,38 +20,35 @@
     
     if(x === 0) return 0
     
-    forbidden = new Set(forbidden)
-    
-    if(forbidden.has(0)) return -1
-    
-    const dp = [[a,1,true]]
-    
-    while(dp.length) {
-        
-            const nextDp = []
-            
-        while(dp.length) {
+    const dp = new Array(x * 2 + 1).fill(1)
 
-        const [pos, jumps, canJumpBack]  = dp.pop()
+    const back = 0, fwd = 1
 
-        if(pos === x) return jumps
-        
-        if(forbidden.has(pos) || !canJumpBack && pos >x || pos > 6000) continue;
-        
-        if(!forbidden.has(pos+a)){
-           nextDp.push([pos+a, jumps+1, true])    
-        }
-        
-        if(canJumpBack && pos - b > 0 && !forbidden.has(pos - b)) {
-            nextDp.push([pos-b, jumps+1, false])
-        }
-            
-        forbidden.add(pos);           
-      }
-        dp.push(...nextDp)
+    dp[0] = 0 
+
+    for(const pos of forbidden) {
+        dp[pos] = -2
     }
-    
-    return -1
+
+    const visitPos = (pos, jumps, dir = fwd) => {
+       
+        if(pos === x) return jumps;
+        
+        if(dp[pos] < 0) return -1
+
+        let  max = Math.max(-1, visitPos(pos+a, jumps+1))
+        dp[pos]--
+
+        if(dir === fwd && dp[pos] > 0)
+
+        max = Math.max(-1, visitPos(pos-a, jumps+1))
+        dp[pos]--
+
+        return max
+    }
+
+
+
 };
 
 let forbidden = [162,118,178,152,167,100,40,74,199,186,26,73,200,127,30,124,193,84,184,36,103,149,153,9,54,154,133,95,45,198,79,157,64,122,59,71,48,177,82,35,14,176,16,108,111,6,168,31,134,164,136,72,98]
