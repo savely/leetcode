c9d3d06fc83a750f1abdf9c07e66fc5521c2d13c
savely <avelikodny@gmail.com>
2021-06-21 12:48:13 +0300

    removed test code

--- longestSubarray.js
+++ longestSubarray.js
@@ -71,9 +71,3 @@ Constraints:
     }
   return maxLen;   
 };
-
-let nums = [10,1,2,4,7,2], limit = 5;
-nums = [4,2,2,2,4,4,2,2], limit = 0;
-//nums = [8,2,4,7], limit = 4;
-
-console.log(longestSubarray(nums, limit))
