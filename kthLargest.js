75600c38c8f54ceaada0c674f6ac501f524f7ea2
savely <avelikodny@gmail.com>
2021-07-04 10:44:32 +0300

    accepted, qiuckselect

--- kthLargest.js
+++ kthLargest.js
@@ -27,36 +27,47 @@ var findKthLargest = function(nums, k) {
 
     if(nums.length < 2) return nums[0];
     
-    const i = quickSelect(nums, 0, nums.length - 1, nums.length - k - 1);
+    res =  quickSelect(nums, 0, nums.length - 1, nums.length - k);
 
-    return nums[i];
+    console.table(nums);
+
+    return res;
+};
+
+const swap  = (arr, i, j) => {
+
+   if(i === j) return;
+
+    const tmp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = tmp;
 };
 
 const quickSelect = (arr, from, to, k) => {
 
-    if(from > to) return undefined;
+    if(from === to) return arr[to];
 
-    if(from === to) return k === 1 ? arr[to] : undefined;
+    const idx = Math.floor(Math.random() * (to - from)) + from;
+    swap(arr, idx, to);
+    let pivot = arr[to];
+    let left = from;
 
-    let pivot = from;
+    for(let i = from; i < to; i++) {
 
-    for(let i = from + 1; i <= to; i++) {
-
-        if(arr[i] < arr[pivot]) {
-            const tmp = arr[i];
-            arr[i] = arr[pivot];
-            arr[pivot] = tmp;
-            pivot = i;
+        if(arr[i] < pivot) {
+            swap(arr, i, left++);
         }
     }
-
-    if(pivot === k) return pivot;
     
-    if(pivot < k) return quickSelect(arr, pivot + 1, to, k - pivot);
+    swap(arr, left, to);
+    if(left === k) return arr[left];
 
-    return quickSelect(arr, from, pivot - 1, k);
+    if(left < k) return quickSelect(arr, left + 1, to, k);
+
+    return quickSelect(arr, from, left - 1, k);
 }
 
 let nums = [3,2,1,5,6,4], k = 2;
+nums = [3,2,3,1,2,4,5,5,6], k = 4;
 
 console.log(findKthLargest(nums, k));
