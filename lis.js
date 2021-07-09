d9cf7fab34f54e5312c3d654e9cb76abddb3f34a
savely <avelikodny@gmail.com>
2021-07-09 11:33:26 +0300

    sequence. nlogn

--- lis.js
+++ lis.js
@@ -8,11 +8,24 @@ var lengthOfLIS = function(nums) {
 
  const search  = (n) => {
 
-        let i = 0;
+       if(seq.length === 1) return 0;
 
-        while(i < seq.length && seq[i] < n) i++;
+        let lo = 0, hi = seq.length -1;
 
-        return i;
+        while(hi > lo)  {
+
+            const mid = (hi + lo) / 2 >> 0, el = seq[mid];
+
+            if(el >= n) {
+                if(seq[mid - 1] < n) return mid;
+                hi = mid - 1;
+            } else {
+                if(seq[mid + 1] >= n) return mid + 1;
+                lo = mid + 1;
+            }
+        }
+
+        return lo;
     }
 
     for(let i = 1; i < nums.length; i++) {
@@ -28,6 +41,9 @@ var lengthOfLIS = function(nums) {
     return seq.length;
 };
 
-let arr = [10,9,2,5,3,7,101,18]
+let arr = [10,9,2,5,3,7,101,18];
+arr = [0,1,0,3,2,3];
+arr = [4,10,4,3,8,9];
+arr = [3,5,6,2,5,4,19,5,6,7,12]; //6
 
 console.log(lengthOfLIS(arr))
