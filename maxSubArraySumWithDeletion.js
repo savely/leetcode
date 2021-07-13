f48fc5434af83a4680a6404029b58a737cd95947
savely <avelikodny@gmail.com>
2021-07-13 13:31:07 +0300

    O(n^2) not accepted

---
+++ maxSubArraySumWithDeletion.js
@@ -0,0 +1,58 @@
+/*
+# 1186. Maximum Subarray Sum with One Deletion
+
+ven an array of integers, return the maximum sum for a non-empty subarray (contiguous elements) with at most one element deletion. In other words, you want to choose a subarray and optionally delete one element from it so that there is still at least one element left and the sum of the remaining elements is maximum possible.
+
+Note that the subarray needs to be non-empty after deleting one element.
+
+ 
+
+Example 1:
+
+Input: arr = [1,-2,0,3]
+Output: 4
+Explanation: Because we can choose [1, -2, 0, 3] and drop -2, thus the subarray [1, 0, 3] becomes the maximum value.
+Example 2:
+
+Input: arr = [1,-2,-2,3]
+Output: 3
+Explanation: We just choose [3] and it's the maximum sum.
+Example 3:
+
+Input: arr = [-1,-1,-1,-1]
+Output: -1
+Explanation: The final subarray needs to be non-empty. You can't choose [-1] and delete -1 from it, then get an empty subarray to make the sum equals to 0.
+ 
+
+Constraints:
+
+1 <= arr.length <= 105
+-104 <= arr[i] <= 104
+*/
+
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+ var maximumSum = function(arr) {
+    
+   
+    let maxSum = -Infinity;
+    
+    for(let i = 0; i < arr.length; i++) {
+        
+        let sum = arr[i];
+        let min = arr[i];
+
+        maxSum = Math.max(maxSum, arr[i]);
+        
+        for(let j = i+1; j < arr.length; j++) {
+            
+            sum += arr[j];
+            min = Math.min(min, arr[j]);
+            maxSum = Math.max(maxSum, sum, sum - min);
+        }
+    }
+    return maxSum;
+};
+
