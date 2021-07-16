dd54fb83d9868a12aa56b2a54d710a54251cd8f1
savely <avelikodny@gmail.com>
2021-07-16 23:00:53 +0300

    accepted O(n) time O(n) memory

--- moveBallsToBoxes.js
+++ moveBallsToBoxes.js
@@ -1,11 +1,15 @@
 /*
 #1769. Minimum Number of Operations to Move All Balls to Each Box
 
-You have n boxes. You are given a binary string boxes of length n, where boxes[i] is '0' if the ith box is empty, and '1' if it contains one ball.
+You have n boxes. You are given a binary string boxes of length n, 
+where boxes[i] is '0' if the ith box is empty, and '1' if it contains one ball.
 
-In one operation, you can move one ball from a box to an adjacent box. Box i is adjacent to box j if abs(i - j) == 1. Note that after doing so, there may be more than one ball in some boxes.
+In one operation, you can move one ball from a box to an adjacent box.
+ Box i is adjacent to box j if abs(i - j) == 1. Note that after doing so, t
+ here may be more than one ball in some boxes.
 
-Return an array answer of size n, where answer[i] is the minimum number of operations needed to move all the balls to the ith box.
+Return an array answer of size n, where answer[i] is the minimum number of operations 
+needed to move all the balls to the ith box.
 
 Each answer[i] is calculated considering the initial state of the boxes.
 
@@ -16,9 +20,12 @@ Example 1:
 Input: boxes = "110"
 Output: [1,1,3]
 Explanation: The answer for each box is as follows:
-1) First box: you will have to move one ball from the second box to the first box in one operation.
-2) Second box: you will have to move one ball from the first box to the second box in one operation.
-3) Third box: you will have to move one ball from the first box to the third box in two operations, and move one ball from the second box to the third box in one operation.
+1) First box: you will have to move one ball from the second box to the first 
+box in one operation.
+2) Second box: you will have to move one ball from the first box to the second box 
+in one operation.
+3) Third box: you will have to move one ball from the first box to the third box 
+in two operations, and move one ball from the second box to the third box in one operation.
 
 Example 2:
 
@@ -41,14 +48,33 @@ Constraints:
  var minOperations = function(boxes) {
     
     const ans = new Array(boxes.length).fill(0);
+    const balls = new Array(boxes.length).fill(0);
+
+    balls[0] = +boxes[0];
+    let moves = 0;
     
-    for(let i = 0; i < boxes.length; i++) {
+    for(let i = 1; i < boxes.length; i++) {
+       
+       moves += balls[i-1];
+       ans[i] = moves;
+       balls[i] = balls[i-1] + (+boxes[i]);
+    }
+
+    moves = 0;
+    balls.fill(0);
+    balls[balls.length - 1] = +boxes[boxes.length - 1];
+
+    for(let i = boxes.length - 2; i >= 0; i--) {
         
-        if(boxes[i] === '0') continue;
-        
-        for(let j = 0; j < ans.length; j++) {
-            ans[j] += Math.abs(i - j);
-        }
-    }    
+        moves += balls[i + 1];
+        balls[i] = balls[i+1] + (+boxes[i]);
+
+        ans[i] += moves;
+    } 
+
     return ans;
 };
+boxes = "111";
+boxes = "001011";
+
+console.table(minOperations(boxes));
