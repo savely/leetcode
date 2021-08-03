255109e7b393f38cf3dfc5e38b5722722775b899
savely <avelikodny@gmail.com>
2021-08-03 12:29:30 +0300

    accepted

--- deleteColumnsLex.js
+++ deleteColumnsLex.js
@@ -1,13 +1,49 @@
 /*
-We are given an array A of N lowercase letter strings, all of the same length.
+# 955. Delete Columns to Make Sorted II
 
-Now, we may choose any set of deletion indices, and for each string, we delete all the characters in those indices.
 
-For example, if we have an array A = ["abcdef","uvwxyz"] and deletion indices {0, 2, 3}, then the final array after deletions is ["bef","vyz"].
+You are given an array of n strings strs, all of the same length.
 
-Suppose we chose a set of deletion indices D such that after deletions, the final array has its elements in lexicographic order (A[0] <= A[1] <= A[2] ... <= A[A.length - 1]).
+We may choose any deletion indices, and we delete all the characters in those indices for each string.
 
-Return the minimum possible value of D.length.
+For example, if we have strs = ["abcdef","uvwxyz"] and deletion indices {0, 2, 3}, then the final array after deletions
+ is ["bef", "vyz"].
+
+Suppose we chose a set of deletion indices answer such that after deletions, the final array has 
+its elements in lexicographic order (i.e., strs[0] <= strs[1] <= strs[2] <= ... <= strs[n - 1]). 
+Return the minimum possible value of answer.length.
+
+ 
+
+Example 1:
+
+Input: strs = ["ca","bb","ac"]
+Output: 1
+Explanation: 
+After deleting the first column, strs = ["a", "b", "c"].
+Now strs is in lexicographic order (ie. strs[0] <= strs[1] <= strs[2]).
+We require at least 1 deletion since initially strs was not in lexicographic order, so the answer is 1.
+Example 2:
+
+Input: strs = ["xc","yb","za"]
+Output: 0
+Explanation: 
+strs is already in lexicographic order, so we do not need to delete anything.
+Note that the rows of strs are not necessarily in lexicographic order:
+i.e., it is NOT necessarily true that (strs[0][0] <= strs[0][1] <= ...)
+Example 3:
+
+Input: strs = ["zyx","wvu","tsr"]
+Output: 3
+Explanation: We have to delete every column.
+ 
+
+Constraints:
+
+n == strs.length
+1 <= n <= 100
+1 <= strs[i].length <= 100
+strs[i] consists of lowercase English letters.
 /*
 
 /**
@@ -16,35 +52,40 @@ Return the minimum possible value of D.length.
  */
 var minDeletionSize = function(A) {
     
-    if(A.length < 2) return 0
-    
-    let count = 0
+    if(A.length < 2) return 0;
 
-    let checkRows = new Array(A.length).fill(false) 
-  
-    
-    for(let j = 0; j < A[0].length; j++) {
-    for (let i = 1; i < A.length; i++) {
-        if(checkRows[i] && A[i][j] <  A[i-1][j]) {
-          count++
-          break
+    const wordLen = A[0].length, deletedRows = new Array(wordLen).fill(false);
+
+    for(let j = 0; j < wordLen; j++) {
+
+        for(let i = 1; i < A.length; i++) {
+
+            const word = A[i], prev = A[i - 1];
+
+            let k = 0;
+
+            while(k < wordLen && (deletedRows[k] || word[k] === prev[k])) {
+                k++;
+            }
+
+            if(k < wordLen) deletedRows[k] = word[k] < prev[k];
         }
 
-       }
-       for (let i = 1; i < A.length; i++) {
-       if (A[i][j] < A[i-1][j])
-           checkRows[i] = true
-       }
-
+        if(deletedRows.filter(x => x) === wordLen) return wordLen;
     }
-    return count
+
+    return deletedRows.filter(x => x).length;
 };
 
 
-console.log(minDeletionSize(["zyx","wvu","tsr"]))
-console.log(minDeletionSize(["xga","xfb","yfa"]))
-//["doeeqiy","yabhbqe","twckqte"]
+// console.log(minDeletionSize(["zyx","wvu","tsr"]));
+// console.log(minDeletionSize(["xga","xfb","yfa"]));
+// //["doeeqiy","yabhbqe","twckqte"]
 
-console.log(minDeletionSize(["doeeqiy","yabhbqe","twckqte"]))
+// console.log(minDeletionSize(["doeeqiy","yabhbqe","twckqte"]));
 
-console.log(minDeletionSize(["doeeqiy","yabhbqe","twckqte"]))
+// console.log(minDeletionSize(["doeeqiy","yabhbqe","twckqte"]));
+
+let  A = ["xga","xfb","yfa"] // 1
+
+console.log(minDeletionSize(A));
