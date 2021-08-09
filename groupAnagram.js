88fc2d16a502b73faf607c0db79344b0d4b90a25
savely <avelikodny@gmail.com>
2021-08-09 13:50:51 +0300

    also accepted

--- groupAnagram.js
+++ groupAnagram.js
@@ -3,9 +3,16 @@ var groupAnagrams = function(strs) {
     const map = {};
      
      for(const str of strs) {
-         
-         let hash = [...str].sort().join('');
-         
+    
+        const arr = new Array(26).fill(0);
+
+        for(let i = 0; i < str.length; i++) {
+
+            arr[str.charCodeAt(i) - 97]++;
+        }
+
+        const hash = arr.join('|');
+       
          if(map[hash] === undefined) map[hash] = [];
          
          map[hash].push(str);
@@ -19,5 +26,3 @@ var groupAnagrams = function(strs) {
      
      return res;
  };
-
-console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
