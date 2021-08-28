0ad072be13b03dd473d73dd72162760b20816049
savely <avelikodny@gmail.com>
2021-08-28 11:45:48 +0300

    #1235 removed testcases

--- maxProfitJobScheduling.js
+++ maxProfitJobScheduling.js
@@ -80,9 +80,3 @@ Constraints:
 
     return f(0);
 };
-
-let startTime = [1,2,3,3], endTime = [3,4,5,6], profit = [50,10,40,70];
-startTime = [1,2,3,4,6], endTime = [3,5,10,6,9], profit = [20,20,100,70,60];
-startTime = [1,1,1], endTime = [2,3,4], profit = [5,6,4];
-
-console.log(jobScheduling(startTime, endTime, profit));
