c1c92b515790b2e42fb61f2c55c281640b63fc7f
savely <avelikodny@gmail.com>
2021-09-01 13:36:27 +0300

    #994. Rotting Oranges updated description

--- rottingOranges.js
+++ rottingOranges.js
@@ -1,22 +1,48 @@
 /*
-#994
-n a given grid, each cell can have one of three values:
+#994. Rotting Oranges
 
-the value 0 representing an empty cell;
-the value 1 representing a fresh orange;
-the value 2 representing a rotten orange.
-Every minute, any fresh orange that is adjacent (4-directionally) to a rotten orange becomes rotten.
+You are given an m x n grid where each cell can have one of three values:
 
-Return the minimum number of minutes that must elapse until no cell has a fresh orange.  If this is impossible, return -1 instead.
+0 representing an empty cell,
+1 representing a fresh orange, or
+2 representing a rotten orange.
+Every minute, any fresh orange that is 4-directionally adjacent to a rotten orange becomes rotten.
+
+Return the minimum number of minutes that must elapse until no cell has a fresh orange. If this is impossible, return -1.
 
  
-*/
+
+Example 1:
+
+
+Input: grid = [[2,1,1],[1,1,0],[0,1,1]]
+Output: 4
+Example 2:
+
+Input: grid = [[2,1,1],[0,1,1],[1,0,1]]
+Output: -1
+Explanation: The orange in the bottom left corner (row 2, column 0) is never rotten, because rotting only happens 4-directionally.
+Example 3:
+
+Input: grid = [[0,2]]
+Output: 0
+Explanation: Since there are already no fresh oranges at minute 0, the answer is just 0.
+ 
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 10
+grid[i][j] is 0, 1, or 2.
+
+ */
 
 /**
  * @param {number[][]} grid
  * @return {number}
  */
-var orangesRotting = function(grid) {
+ var orangesRotting = function(grid) {
     
     const h = grid.length - 1, w = grid[0].length - 1, nextGen = new Set()
     let iter = 0, hasFresh = false
@@ -68,9 +94,12 @@ var orangesRotting = function(grid) {
   return hasFresh ? -1 : iter
 };
 
-let grid = [[2,1,1],[1,1,0],[0,1,1]]
-//grid = [[0,2]]
-//grid = [[2,1,1],[0,1,1],[1,0,1]]
+let grid = [[2,1,1],[1,1,0],[0,1,1]];
+//grid = [[0,2]];
+/*grid = [[2,1,1],[0,1,1],[1,0,1]];
+grid = [[2,1,1],
+        [1,1,1],
+        [0,1,2]];//2*/
 
 
-console.log(orangesRotting(grid))
+console.log(orangesRotting(grid));
