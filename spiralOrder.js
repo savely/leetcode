179778182da6267bdefd9c282a6bbfcbede0294d
savely <avelikodny@gmail.com>
2021-09-16 12:44:40 +0300

    accepted

--- spiralOrder.js
+++ spiralOrder.js
@@ -24,3 +24,36 @@ Constraints:
     1 <= m, n <= 10
     -100 <= matrix[i][j] <= 100
 */
+
+
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+ var spiralOrder = function(matrix) {
+    
+    let [left, right, top, bottom] = [0, matrix[0].length - 1, 0, matrix.length - 1];
+
+    let arr = [];
+
+    while(left<=right && top <= bottom){
+
+        for(let i=left; i<=right; i++) arr.push(matrix[top][i])
+        top++;
+
+        for(let i=top; i<=bottom; i++) arr.push(matrix[i][right])
+        right--;
+
+        if(top<=bottom){               // condition 1
+            for(let i=right; i>=left; i--) arr.push(matrix[bottom][i])
+            bottom--;
+        }
+      
+
+        if(left<=right){               // condition 2
+            for(let i=bottom; i>=top; i--) arr.push(matrix[i][left])
+            left++;
+        }  
+    }
+    return arr;
+};
