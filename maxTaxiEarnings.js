100e7c72efbf4ab6ff3d93a2618477f20cec43b5
savely <avelikodny@gmail.com>
2021-09-19 11:00:35 +0300

    #2008 - removed testcases

--- maxTaxiEarnings.js
+++ maxTaxiEarnings.js
@@ -108,8 +108,3 @@ Constraints:
 
     return ans;
 };
-
-let n = 5, rides = [[2,5,4],[1,5,1]];
-n = 20, rides = [[1,6,1],[3,10,2],[10,12,3],[11,12,2],[12,15,2],[13,18,1]];
-
-console.log(maxTaxiEarnings(n, rides));
