d698193c7093ea06c8302e4df7a66add8f58264f
savely <avelikodny@gmail.com>
2021-09-22 11:56:37 +0300

    #1143. Longest Common Subsequence - 2D DP, accepted

--- longestCommonSubSequence.js
+++ longestCommonSubSequence.js
@@ -43,22 +43,21 @@ text1 and text2 consist of only lowercase English characters.
  */
 var longestCommonSubsequence = function(text1, text2) {
 
-    
-    const dp = new Array(text1.length+1).fill(0).map((_,i) => {
-        arr = new Array(text2.length+1)
-        arr.fill(0)
-       return arr
-    })
+    const dp = new Array(text1.length + 1).fill(0).map(_ => new Array(text2.length + 1). fill(0));
 
-    for(let i = 1; i <= text1.length; i++) {
-        for (let j = 1; j <= text2.length; j++) {
-            if(text1[i-1] === text2[j-1]) {
-                dp[i][j] = dp[i-1][j-1] +1
+    for(let i = 1; i < dp.length; i++) {
+
+        for(let j = 1; j < dp[0].length; j++) {
+
+            if(text1[i - 1] === text2[j - 1]) {
+                dp[i][j] = 1 + dp[i - 1][j - 1];
             } else {
-                dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1])
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
             }
         }
     }
 
-    return dp[text1.length][text2.length]
+    console.table(dp);
+
+    return dp[text1.length][text2.length];
 };
