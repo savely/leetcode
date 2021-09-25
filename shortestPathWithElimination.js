f89b405fbe7b31edbf9538bd391d4e782956c4ab
savely <avelikodny@gmail.com>
2021-09-25 14:07:25 +0300

    #1293. Shortest Path in a Grid with Obstacles Elimination - BFS, O((m + n) * k), accepted

---
+++ shortestPathWithElimination.js
@@ -0,0 +1,90 @@
+/*
+#1293. Shortest Path in a Grid with Obstacles Elimination
+
+You are given an m x n integer matrix grid where each cell is either 0 (empty) or 1 (obstacle). 
+You can move up, down, left, or right from and to an empty cell in one step.
+
+Return the minimum number of steps to walk from the upper left corner (0, 0) to the lower right corner (m - 1, n - 1) 
+given that you can eliminate at most k obstacles. If it is not possible to find such walk return -1.
+
+ 
+
+Example 1:
+
+Input: 
+grid = 
+[[0,0,0],
+ [1,1,0],
+ [0,0,0],
+ [0,1,1],
+ [0,0,0]], 
+k = 1
+Output: 6
+Explanation: 
+The shortest path without eliminating any obstacle is 10. 
+The shortest path with one obstacle elimination at position (3,2) is 6. Such path is (0,0) -> (0,1) -> (0,2) -> (1,2) -> (2,2) -> (3,2) -> (4,2).
+Example 2:
+
+Input: 
+grid = 
+[[0,1,1],
+ [1,1,1],
+ [1,0,0]], 
+k = 1
+Output: -1
+Explanation: 
+We need to eliminate at least two obstacles to find such a walk.
+ 
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 40
+1 <= k <= m * n
+grid[i][j] == 0 or 1
+grid[0][0] == grid[m - 1][n - 1] == 0
+
+*/
+
+
+/**
+ * @param {number[][]} grid
+ * @param {number} k
+ * @return {number}
+ */
+
+ var shortestPath = function(grid, k) {
+
+    const m = grid.length, n = grid[0].length;
+
+    if(k >= m + n - 2) return m + n - 2;
+
+    const queue = [[0,0,k,0]], visited = new Set([`0:0:${k}`]);
+
+    let steps = 0;
+
+    while(queue.length) {
+
+        const [x, y, eliminate, steps] = queue.shift();
+
+        for(const [i, j] of [[x+1,y],[x-1,y],[x,y+1],[x,y-1]]) {
+
+            if(i === m - 1 && j === n -1 && (eliminate - grid[i][j] >= 0)) return steps + 1;
+
+            if(i < 0 || i >= m) continue;
+            if(j < 0 || j >= n) continue;
+
+            const elmnt = eliminate - grid[i][j];
+
+            if(elmnt < 0 || visited.has(`${i}:${j}:${elmnt}`)) continue;
+
+            visited.add(`${i}:${j}:${elmnt}`);
+
+            queue.push([i, j, elmnt, steps + 1]);
+         }
+
+    }
+
+    return -1;
+};
