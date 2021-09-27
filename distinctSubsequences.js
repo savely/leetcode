390e8ba8c6f32af9248ad0b610c89ff669fac0f5
savely <avelikodny@gmail.com>
2021-09-27 12:38:15 +0300

    Unique Email Addresses - dp, accepted

---
+++ distinctSubsequences.js
@@ -0,0 +1,69 @@
+/*
+c
+
+iven two strings s and t, return the number of distinct subsequences of s which equals t.
+
+A string's subsequence is a new string formed from the original string by deleting some (can be none) 
+of the characters without disturbing the remaining characters' relative positions. 
+(i.e., "ACE" is a subsequence of "ABCDE" while "AEC" is not).
+
+It is guaranteed the answer fits on a 32-bit signed integer.
+
+ 
+
+Example 1:
+
+Input: s = "rabbbit", t = "rabbit"
+Output: 3
+Explanation:
+As shown below, there are 3 ways you can generate "rabbit" from S.
+rabbbit
+rabbbit
+rabbbit
+
+Example 2:
+
+Input: s = "babgbag", t = "bag"
+Output: 5
+Explanation:
+As shown below, there are 5 ways you can generate "bag" from S.
+babgbag
+babgbag
+babgbag
+babgbag
+babgbag
+
+ 
+
+Constraints:
+
+    1 <= s.length, t.length <= 1000
+    s and t consist of English letters.
+
+*/
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {number}
+ */
+ var numDistinct = function(s, t) {
+
+    const dp = new Array(t.length + 1).fill(0).map(_ => new Array(s.length + 1).fill(0));
+
+    dp[0].fill(1);
+
+    for(let j = 1; j <= s.length; j++) {
+
+        for(let i = 1; i <= t.length; i++) {
+
+            if(s[j - 1] === t[i - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + dp[i][j - 1];
+            } else {
+                dp[i][j] = dp[i][j - 1];
+            }
+        }
+    }
+
+    return dp[t.length][s.length];
+};
