3045486567906083fce7913cdc2dbf5f3142dd6e
savely <avelikodny@gmail.com>
2021-10-18 16:47:08 +0300

    #1319. Number of Operations to Make Network Connected - union find, accepted

--- minOpsMakeNetworkConnected.js
+++ minOpsMakeNetworkConnected.js
@@ -68,9 +68,14 @@ Constraints:
         const p1 = find(node1), p2 = find(node2);
         
         if(p1 === p2) return 1;
-        
-        dSet[node2] = p1;
-        dSet[p1]--;
+
+        if(dSet[p1] <= dSet[p2]) {
+            dSet[p2] = p1;
+            dSet[p1]--;
+        } else {
+            dSet[p1] = p2;
+            dSet[p2]--;            
+        }    
         
         return 0;
     }
@@ -83,9 +88,9 @@ Constraints:
     
     let disconnected = -1;
     
-    console.log(numCables);
+    //console.log(numCables);
     
-    console.table(dSet);
+    //console.table(dSet);
     
     for(const p of dSet) {
         disconnected += p < 0 ? 1 : 0;
@@ -100,7 +105,6 @@ let n = 6, connections = [[0,1],[0,2],[0,3],[1,2],[1,3]];
 
 n = 5, connections = [[0,1],[0,2],[3,4],[2,3]];
 
-n = 12;
-connections = [[1,5],[1,7],[1,2],[1,4],[3,7],[4,7],[3,5],[0,6],[0,1],[0,4],[2,6],[0,3],[0,2]]; //4
+n = 12, connections = [[1,5],[1,7],[1,2],[1,4],[3,7],[4,7],[3,5],[0,6],[0,1],[0,4],[2,6],[0,3],[0,2]]; //4
 
 console.log(makeConnected(n, connections));
