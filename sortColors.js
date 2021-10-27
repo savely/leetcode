5dbe94acba5c53dc50e985c890f6489f23e37fc2
savely <avelikodny@gmail.com>
2021-10-27 09:04:09 +0300

    #75. Sort Colors - moved to seprate file

---
+++ sortColors.js
@@ -0,0 +1,80 @@
+/*
+#75. Sort Colors
+
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+You must solve this problem without using the library's sort function.
+
+ 
+
+Example 1:
+
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+Example 2:
+
+Input: nums = [2,0,1]
+Output: [0,1,2]
+Example 3:
+
+Input: nums = [0]
+Output: [0]
+Example 4:
+
+Input: nums = [1]
+Output: [1]
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 300
+nums[i] is 0, 1, or 2.
+
+*/
+
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+ var sortColors = function(nums) {
+
+    if(nums.length < 2) return nums
+
+     let red = 0, white = 1, blue = 2
+     let lo = 0, mid = 0,  hi = nums.length - 1
+     
+     const swap = function(i,j) {
+       const tmp = nums[i]
+       nums[i] = nums[j]
+       nums[j] = tmp
+     }
+
+     while(mid <= hi) {
+         if(nums[mid] === red) {
+            swap(lo,mid)
+             lo++
+             mid++
+             continue
+         }
+         
+         if(nums[mid] === white) {
+           mid++
+           continue  
+         }
+
+        swap(mid, hi) 
+        hi--  
+     }
+
+};
+
+const toColors = c => c == 0 ? 'r' :(c == 1 ? 'w' : 'b')
+const xs = [2,0,2,1,1,0,0,1,2]
+console.log(xs.map(toColors))
+console.log('------------------------------')
+sortColors(xs)
+console.log(xs.map(toColors))
