e7ee87694835134252541c69c7aecc9ed32334e6
savely <avelikodny@gmail.com>
2021-12-02 11:59:43 +0200

    #328. Odd Even Linked List - accepted

--- oddEvenList.js
+++ oddEvenList.js
@@ -1,44 +1,62 @@
-function ListNode(val) {
-    this.val = val;
-    this.next = null;
-}
+/*
+#328. Odd Even Linked List
 
-function arrayToList(arr) {
-    if (arr.length === 0)
-        return null;
-    var head = new ListNode(arr.shift());
-    var a2l = function (arr, node) {
-        if (arr.length === 0)
-            return;
-        var nextNode = new ListNode(arr.shift());
-        node.next = nextNode;
-        a2l(arr, nextNode);
-    };
-    a2l(arr, head);
-    return head;
-}
+Given the head of a singly linked list, group all the nodes with odd indices together followed by the nodes with even indices, and return the reordered list.
 
-function printList(list) {
-    if (typeof list.val === 'undefined') {
-        return "";
-    }
-    if (list.next === null) {
-        return "(" + list.val + ")";
-    }
-    return "(" + list.val + ")->" + printList(list.next);
-}
+The first node is considered odd, and the second node is even, and so on.
+
+Note that the relative order inside both the even and odd groups should remain as it was in the input.
+
+You must solve the problem in O(1) extra space complexity and O(n) time complexity.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [1,3,5,2,4]
+Example 2:
+
+
+Input: head = [2,1,3,5,6,4,7]
+Output: [2,3,6,7,1,5,4]
+ 
+
+Constraints:
+
+n == number of nodes in the linked list
+0 <= n <= 104
+-106 <= Node.val <= 106
+*/
+
+const {arrayToList, printList} = require("./listUtil");
 
 var oddEvenList = function(head) {
-  
+    
     if(head === null || head.next === null) return head
+    
+    let odd = head
+    let even = head.next
+    const evenStart = even
 
     
+    while(even !== null && even.next !== null) {
+        odd.next = even.next
+        odd = odd.next
+        even.next = odd.next
+        even = even.next
+    }
+    
+    odd.next = evenStart
+    
+    return head
 };
 
 
-const a1 =[1,2,3,4,5]
-const l = arrayToList(a1)
-console.log(printList(l))
+const a1 =[1,2,3,4,5];
+const l = arrayToList(a1);
+console.log(printList(l));
 console.log('----------------------')
-oddEvenList(l)
-console.log(printList(l))
+oddEvenList(l);
+console.log(printList(l));
