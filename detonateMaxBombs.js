a7a093a43942434757a2d0f4a169baf3eae2874d
savely <avelikodny@gmail.com>
2021-12-12 21:22:05 +0200

    #2101. Detonate the Maximum Bombs - dfs, accepted

---
+++ detonateMaxBombs.js
@@ -0,0 +1,117 @@
+/*
+#2101. Detonate the Maximum Bombs
+
+You are given a list of bombs. The range of a bomb is defined as the area where its effect can be felt. This area is in the shape of a circle with the center as the location of the bomb.
+
+The bombs are represented by a 0-indexed 2D integer array bombs where bombs[i] = [xi, yi, ri]. xi and yi denote the X-coordinate and Y-coordinate of the location of the ith bomb, whereas ri denotes the radius of its range.
+
+You may choose to detonate a single bomb. When a bomb is detonated, it will detonate all bombs that lie in its range. These bombs will further detonate the bombs that lie in their ranges.
+
+Given the list of bombs, return the maximum number of bombs that can be detonated if you are allowed to detonate only one bomb.
+
+ 
+
+Example 1:
+
+
+Input: bombs = [[2,1,3],[6,1,4]]
+Output: 2
+Explanation:
+The above figure shows the positions and ranges of the 2 bombs.
+If we detonate the left bomb, the right bomb will not be affected.
+But if we detonate the right bomb, both bombs will be detonated.
+So the maximum bombs that can be detonated is max(1, 2) = 2.
+Example 2:
+
+
+Input: bombs = [[1,1,5],[10,10,5]]
+Output: 1
+Explanation:
+Detonating either bomb will not detonate the other bomb, so the maximum number of bombs that can be detonated is 1.
+Example 3:
+
+
+Input: bombs = [[1,2,3],[2,3,1],[3,4,2],[4,5,3],[5,6,4]]
+Output: 5
+Explanation:
+The best bomb to detonate is bomb 0 because:
+- Bomb 0 detonates bombs 1 and 2. The red circle denotes the range of bomb 0.
+- Bomb 2 detonates bomb 3. The blue circle denotes the range of bomb 2.
+- Bomb 3 detonates bomb 4. The green circle denotes the range of bomb 3.
+Thus all 5 bombs are detonated.
+ 
+
+Constraints:
+
+1 <= bombs.length <= 100
+bombs[i].length == 3
+1 <= xi, yi, ri <= 105
+
+*/
+
+/**
+ * @param {number[][]} bombs
+ * @return {number}
+ */
+ var maximumDetonation = function(bombs) {
+
+    const dist = ([x1, y1], [x2, y2]) => Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
+
+    const adj = {};
+
+    for(let i = 0; i < bombs.length; i++) {
+
+        const [x1, y1, r1] = bombs[i];
+
+        adj[i] = adj[i] || [];
+
+        for(let j = 0; j < bombs.length; j ++) {
+
+            if(i === j) continue;
+
+            adj[j] = adj[j] || [];
+
+            const [x2, y2] = bombs[j];
+
+            if(dist([x1, y1], [x2, y2]) <= r1) adj[i].push(j);
+        }
+    }
+
+    const detonated = new Set();
+
+    const dfs =(i) => {
+
+        if(i >= bombs.length || detonated.has(i)) return 0;
+
+        detonated.add(i);
+
+        let count = 1;
+
+        for(const j of adj[i]) {
+            count += dfs(j);
+        }
+
+        return count;
+    };
+
+    let max = 1;
+    
+    for(let i = 0; i < bombs.length; i++) {
+
+        detonated.clear();
+
+        max = Math.max(max, dfs(i));
+    }
+
+    return max;
+};
+
+let bombs = [[2,1,3],[6,1,4]];
+//bombs = [[1,1,5],[10,10,5]];
+//bombs = [[1,2,3],[2,3,1],[3,4,2],[4,5,3],[5,6,4]];
+
+/*bombs = [[855,82,158],[17,719,430],[90,756,164],[376,17,340],[691,636,152],[565,776,5],[464,154,271],
+         [53,361,162],[278,609,82],[202,927,219],[542,865,377],[330,402,270],[720,199,10],[986,697,443],
+         [471,296,69],[393,81,404],[127,405,177]];*/
+
+console.log(maximumDetonation(bombs));
