7136f2219aa417449316a5d9e0d3dd577485325e
savely <avelikodny@gmail.com>
2021-12-19 11:05:46 +0200

    #394. Decode String - stacks, accepted; polish.js renamed to decodeString.js

---
+++ decodeString.js
@@ -0,0 +1,96 @@
+/*
+#394. Decode String
+
+Given an encoded string, return its decoded string.
+
+The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times. 
+Note that k is guaranteed to be a positive integer.
+
+You may assume that the input string is always valid; No extra white spaces, square brackets are well-formed, etc.
+
+Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k. 
+For example, there won't be input like 3a or 2[4].
+
+ 
+
+Example 1:
+
+Input: s = "3[a]2[bc]"
+Output: "aaabcbc"
+Example 2:
+
+Input: s = "3[a2[c]]"
+Output: "accaccacc"
+Example 3:
+
+Input: s = "2[abc]3[cd]ef"
+Output: "abcabccdcdcdef"
+Example 4:
+
+Input: s = "abc3[cd]xyz"
+Output: "abccdcdcdxyz"
+ 
+
+Constraints:
+
+1 <= s.length <= 30
+s consists of lowercase English letters, digits, and square brackets '[]'.
+s is guaranteed to be a valid input.
+All the integers in s are in the range [1, 300].
+
+*/
+
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+
+ 
+var decodeString = function(s) {
+    
+  const nums = [], stack = [];
+
+  let currNum = 0;
+
+  for(const ch of s) {
+
+    if(!Number.isNaN(+ch)) {
+      currNum = currNum * 10 + +ch;
+      continue;
+    }
+
+    if(ch === '[') {
+      nums.push(currNum);
+      currNum = 0;
+      stack.push(ch);
+      continue;
+    }
+
+    if(ch === ']') {
+
+      const times = nums.pop();
+
+      const str = [];
+
+      while(stack[stack.length - 1] !== '[') {
+
+        str.push(stack.pop());
+      }
+
+      stack.pop(); // remove '['
+
+      for(let i = 1; i <= times; i++) {
+
+        for(let j = str.length - 1; j >= 0; j--) {
+
+          stack.push(str[j]);
+        }
+      }
+
+      continue;
+    }
+    stack.push(ch);
+  }
+
+  return stack.join('');
+};
