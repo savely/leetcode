d914e35f152f5ed4a8b1d796a61650319a73caef
savely <avelikodny@gmail.com>
2022-02-04 10:38:52 +0200

    #525. Contiguous Array - hashmap, accepted

--- contiguous.js
+++ contiguous.js
@@ -1,25 +1,51 @@
+/*
+#525. Contiguous Array
+
+Given a binary array nums, return the maximum length of a contiguous subarray with an equal number of 0 and 1.
+
+ 
+
+Example 1:
+
+Input: nums = [0,1]
+Output: 2
+Explanation: [0, 1] is the longest contiguous subarray with an equal number of 0 and 1.
+Example 2:
+
+Input: nums = [0,1,0]
+Output: 2
+Explanation: [0, 1] (or [1, 0]) is a longest contiguous subarray with equal number of 0 and 1.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+nums[i] is either 0 or 1.
+
+*/
+
 /**
  * @param {number[]} nums
  * @return {number}
  */
-var findMaxLength = function(nums) {
+ var findMaxLength = function(nums) {
 
-    const sums = {}
-    let sum = 0
-    let max = 0
- 
-    for(let i = 0; i < nums.length; i++) {
-        
-     sum += nums[i] === 1 ? 1 : -1
-        
-     if(sum === 0) {
-       max = i + 1
-     } else if (sums[sum] === undefined){
-       sums[sum] = i+1                                                                                                                 } else {
-       max = Math.max(max, i - sums[sum] + 1)
-     }                                                                                                                                                  
+  const map = {0 : -1};
+
+  let max = 0, balance = 0;
+
+
+  for(let i = 0; i < nums.length; i++) {
+
+    balance += nums[i] ? 1 : -1;
+
+    if(map[balance] === undefined) {
+      map[balance] = i;
+      continue;
     }
-    return max
- };
 
- console.log(findMaxLength([0,0,1]))
+    max = Math.max(max, i - map[balance]);
+  }
+
+  return max;
+};
