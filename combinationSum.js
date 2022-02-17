02691f4b7ed0a4882cf758515402bb3860078843
savely <avelikodny@gmail.com>
2022-02-17 08:36:35 +0200

    # 39. Combination Sum - added task description

--- combinationSum.js
+++ combinationSum.js
@@ -1,3 +1,42 @@
+/*
+# 39. Combination Sum
+
+Given an array of distinct integers candidates and a target integer target, return a list of all unique combinations of candidates where the chosen numbers sum to target. You may return the combinations in any order.
+
+The same number may be chosen from candidates an unlimited number of times. Two combinations are unique if the frequency of at least one of the chosen numbers is different.
+
+It is guaranteed that the number of unique combinations that sum up to target is less than 150 combinations for the given input.
+
+ 
+
+Example 1:
+
+Input: candidates = [2,3,6,7], target = 7
+Output: [[2,2,3],[7]]
+Explanation:
+2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+7 is a candidate, and 7 = 7.
+These are the only two combinations.
+Example 2:
+
+Input: candidates = [2,3,5], target = 8
+Output: [[2,2,2,2],[2,3,3],[3,5]]
+Example 3:
+
+Input: candidates = [2], target = 1
+Output: []
+ 
+
+Constraints:
+
+1 <= candidates.length <= 30
+1 <= candidates[i] <= 200
+All elements of candidates are distinct.
+1 <= target <= 500
+*/
+
+
+
 /**
  * @param {number[]} candidates
  * @param {number} target
@@ -7,28 +46,26 @@
  
 var combinationSum = function(candidates, target) {
 
-    const sorted = candidates.sort((a,b) => a-b)
-    const cand = []
-    const res = []
+    const sorted = candidates.sort((a,b) => a-b);
+    const cand = [];
+    const res = [];
 
     const backTrack = function(idx,tgt) {
          
-        if(tgt < 0 ) return
+        if(tgt < 0 ) return;
 
         if( tgt === 0) {
-            res.push(Array.from(cand))
-            return 
+            res.push(Array.from(cand));
+            return;
         }
 
         for(let i = idx; i < sorted.length && sorted[i] <= tgt; i++) {
-          cand.push(sorted[i])
-          backTrack(i, tgt - sorted[i])
-          cand.pop()
+          cand.push(sorted[i]);
+          backTrack(i, tgt - sorted[i]);
+          cand.pop();
         }
     }
-    backTrack(0, target)
+    backTrack(0, target);
 
-    return res
+    return res;
 };
-
-//console.log(combinationSum([2,3,5], 8))
