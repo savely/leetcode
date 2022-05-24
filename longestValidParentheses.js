58f295953d63a2aa644c6a8e754d1b98fc9f374b
savely <avelikodny@gmail.com>
2022-05-24 14:22:03 +0200

    #32. Longest Valid Parentheses -stack. accepted

--- longestValidParentheses.js
+++ longestValidParentheses.js
@@ -1,39 +1,65 @@
+/*
+#32. Longest Valid Parentheses
+
+Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
+
+ 
+
+Example 1:
+
+Input: s = "(()"
+Output: 2
+Explanation: The longest valid parentheses substring is "()".
+
+Example 2:
+
+Input: s = ")()())"
+Output: 4
+Explanation: The longest valid parentheses substring is "()()".
+
+Example 3:
+
+Input: s = ""
+Output: 0
+
+ 
+
+Constraints:
+
+    0 <= s.length <= 3 * 104
+    s[i] is '(', or ')'.
+
+
+*/
+
 /**
  * @param {string} s
  * @return {number}
  */
  var longestValidParentheses = function(s) {
     
-    if(s.length < 2) return 0
-    
-    let max = 0, i = 0
+    const stack = [-1];
 
-    const dp = new Array(s.length + 1).fill(-1)
+    let max = 0;
 
-    
-    
-    const stack = []
-    
-    while(i < s.length) {
-        
-        if(s[i] === '(') {
-          stack.push(i++)
-          continue
-        }
-        
-        if(stack.length === 0) {
-            i++
-            continue
-        }
+    for (let i = 0; i < s.length; i++) {
 
-      const pos = stack.pop(), curr = i - pos + 1
-      dp[i+1] = Math.max(curr, curr + dp[pos])
-      max = Math.max(max, dp[i+1])
-      i++
+      if(s[i] === '(') {
+        stack.push(i);
+        continue;
+      }
+
+      stack.pop();
+
+      if(stack.length) {
+        max = Math.max(max, i - stack[stack.length - 1]);
+      } else {
+        stack.push(i);
+      }
     }
-    
-    return  max
+
+    return max;
 };
 
-const arr = ["(()", ")()())", "(())())"]
-console.table(arr.map(longestValidParentheses))
+const arr = ["(()", ")()())", "(())())", "()(()"];
+console.table(arr.map(longestValidParentheses));
