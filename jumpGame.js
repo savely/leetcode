585a5447ad623f64acfbb8724bd0f774b9ef768d
savely <avelikodny@gmail.com>
2022-12-26 09:22:01 +0100

    #55. Jump Game - right to left, accepted

--- jumpGame.js
+++ jumpGame.js
@@ -1,37 +1,47 @@
-var canJump = function(nums) {
- 
-    const dp = Array(nums.length).fill(false)
-    const len = nums.length-1
-    dp[len] = true
+/*
+#55. Jump Game
 
-    for(let i = len-1; i >=0; i--) {
-       const jumps = Math.min(len-i, nums[i])
+You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
 
-       for(let j = 1; j <= jumps; j++) {
-           if(dp[i+j]) {
-              dp[i] = true
-              break 
-           }
-       }
-    }
+Return true if you can reach the last index, or false otherwise.
 
-    return dp[0]
-};
+ 
 
-const canJump2 = function(nums) {
+Example 1:
 
-    const len = nums.length-1
-    let left = len
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+Example 2:
+
+Input: nums = [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+
+ 
+
+Constraints:
+
+    1 <= nums.length <= 104
+    0 <= nums[i] <= 105
+
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+
+const canJump = function(nums) {
+
+    const len = nums.length-1;
+    let left = len;
 
     for(let i = len-1; i >=0; i--) {
         if(i + nums[i] >= left) {
-            left = i
+            left = i;
         }
     }
-    return left === 0
+    return left === 0;
 }
-
-let board = [2,3,1,1,4]
-board = [3,2,1,0,4]
-
-console.log(canJump2(board))
