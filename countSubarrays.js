c0e6516a44f6aee4c6723b8082c8b584f0d8b3c1
savely <avelikodny@gmail.com>
2023-06-15 22:38:36 +0200

    #1161. Maximum Level Sum of a Binary Tree - bfs, accepted

---
+++ countSubarrays.js
@@ -0,0 +1,73 @@
+/*
+#2444. Count Subarrays With Fixed Bounds
+
+You are given an integer array nums and two integers minK and maxK.
+
+A fixed-bound subarray of nums is a subarray that satisfies the following conditions:
+
+    The minimum value in the subarray is equal to minK.
+    The maximum value in the subarray is equal to maxK.
+
+Return the number of fixed-bound subarrays.
+
+A subarray is a contiguous part of an array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,5,2,7,5], minK = 1, maxK = 5
+Output: 2
+Explanation: The fixed-bound subarrays are [1,3,5] and [1,3,5,2].
+
+Example 2:
+
+Input: nums = [1,1,1,1], minK = 1, maxK = 1
+Output: 10
+Explanation: Every subarray of nums is a fixed-bound subarray. There are 10 possible subarrays.
+
+ 
+
+Constraints:
+
+    2 <= nums.length <= 105
+    1 <= nums[i], minK, maxK <= 106
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} minK
+ * @param {number} maxK
+ * @return {number}
+ */
+var countSubarrays = function(nums, minK, maxK) {
+
+    let countMin = 0, countMax = 0 ,count = 0;
+
+    let i = 0, j = 0;
+
+    while(i < nums.length) {
+
+        if(j === nums.length || nums[j] < minK || nums[j] > maxK) {
+
+            while(i < j) {
+                countMin -= nums[i] === minK ? 1 : 0;
+                countMax -= nums[i] === maxK ? 1 : 0;
+                count += (countMax > 0 && countMin > 0) ? 1 : 0;
+                i++;
+            }
+            j++;
+            i = j;
+        }
+        countMin += nums[j] === minK ? 1 : 0;
+        countMax += nums[j] === maxK ? 1 : 0;
+        count += (countMax > 0 && countMin > 0) ? 1 : 0;
+        j++;
+    }
+    return count;
+};
+
+let nums = [1,3,5,2,7,5], minK = 1, maxK = 5;
+nums = [1,1,1,1], minK = 1, maxK = 1;
+
+console.log(countSubarrays(nums, minK, maxK));
