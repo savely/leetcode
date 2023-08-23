e49c391c849c94a54a654d0f526c9c0585532ef3
savely <avelikodny@gmail.com>
2023-08-23 11:09:48 +0200

    #767. Reorganize String - priority queue, accepted

--- reorganizeString.js
+++ reorganizeString.js
@@ -1,3 +1,35 @@
+/*
+#767. Reorganize String
+
+Given a string s, rearrange the characters of s so that any two adjacent characters are not the same.
+
+Return any possible rearrangement of s or return "" if not possible.
+
+ 
+
+Example 1:
+
+Input: s = "aab"
+Output: "aba"
+
+Example 2:
+
+Input: s = "aaab"
+Output: ""
+
+ 
+
+Constraints:
+
+    1 <= s.length <= 500
+    s consists of lowercase English letters.
+
+
+*/
+
+
+const { PriorityQueue }  = require('@datastructures-js/priority-queue');
+
 /**
  * @param {string} S
  * @return {string}
@@ -5,60 +37,50 @@
  var reorganizeString = function(S) {
     
     const freq = [...S].reduce((acc, n) => {
-       if(!acc[n]) {
-           acc[n] = 0
-       }
-       acc[n]++
-       return acc
-   }, {})
+        acc[n] = (acc[n] || 0) + 1;
+       return acc;
+   }, {});
+
+   const queue = new PriorityQueue({compare : ([pr1, char1], [pr2, char2]) => pr2 - pr1});
     
- 
-    const freqArr = []
+   for(const char in freq) {
+        queue.enqueue([freq[char], char]);
+   }
+
+   let left = S.length, ans = [];
+
+   while(queue.size()) {
     
-    for(const char in freq) {
-        freqArr.push([char, freq[char]])
+    let [fr1, ch1] = queue.dequeue();
+
+    if(fr1 > left) return "";
+
+    if(queue.size()) {
+        let [fr2, ch2] = queue.dequeue();
+        const arr = ans[ans.length - 1] === ch1 ? [ch2, ch1] : [ch1, ch2];
+        ans.push(...arr);
+        fr1--;
+        fr2--;
+        left -= 2;
+        if(fr1 > 0) queue.enqueue([fr1, ch1]);
+        if(fr2 > 0) queue.enqueue([fr2, ch2]);
+    } else {
+
+        if(left > 1) return "";
+        ans.push(ch1);
+        fr1--;
+        if(fr1 > 0) queue.enqueue([fr1, ch1]);
+        left--;
     }
-     
-     if(freqArr.length === 1) return freqArr[0][1] === 1 ? freqArr[0][0] : ""
-     
-     const sortFunc = ([c1, fr1], [c2, fr2]) => fr1 === fr2 ? c2.charCodeAt(0) - c1.charCodeAt(0) : fr2 - fr1  
-    freqArr.sort(sortFunc) 
-     
-     console.table(freqArr)
-     
-     const res =[]
-  
-     while(freqArr.length > 1) {
-         const first = freqArr.shift()
-         
-         if(first[1] > Math.ceil((S.length - res.length) / 2)) return ""
-         
-         const second = freqArr.shift()
-         
-          while(second[1] > 0) {
-              res.push(first[0], second[0])
-              first[1] -= 1
-              second[1] -= 1
-          }
-         
-          if(first[1] > 0) {
-              freqArr.push(first)
-              freqArr.sort(sortFunc) 
-          } 
-     }
+   }
 
-     if(freqArr.length) {
-         const [char, count] = freqArr.pop()
-
-         if(count > 1) return ""
-
-         res.push(char)
-     }
-     
-     console.log(S.length, res.length)
-     return res.join('')
+   return ans.join('');
  };
 
- let str = "asdasdafjakdfdkdfvurbddddf"
- let str2 = 'sgfsdgfgjfdereddresddds'
- console.log(reorganizeString(str2))
+ let s = "asdasdafjakdfdkdfvurbddddf";
+//s = 'sgfsdgfgjfdereddresddds';
+//s = "aab";
+//s= "aabab";
+s = "aabbcc"
+
+ console.log(reorganizeString(s));
