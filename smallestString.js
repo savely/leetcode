118664e27e96e97af20d34cd2528fcdda02c3cba
savely <avelikodny@gmail.com>
2024-04-07 13:27:02 +0200

    #3106. Lexicographically Smallest String After Operations With Constraint - greedy, accepted

---
+++ smallestString.js
@@ -0,0 +1,89 @@
+/*
+#3106. Lexicographically Smallest String After Operations With Constraint
+
+You are given a string s and an integer k.
+
+Define a function distance(s1, s2) between two strings s1 and s2 of the same length n as:
+
+    The sum of the minimum distance between s1[i] and s2[i] when the characters from 'a' to 'z' are placed in a cyclic order, for all i in the range [0, n - 1].
+
+For example, distance("ab", "cd") == 4, and distance("a", "z") == 1.
+
+You can change any letter of s to any other lowercase English letter, any number of times.
+
+Return a string denoting the
+lexicographically smallest
+string t you can get after some changes, such that distance(s, t) <= k.
+
+ 
+
+Example 1:
+
+Input: s = "zbbz", k = 3
+
+Output: "aaaz"
+
+Explanation:
+
+Change s to "aaaz". The distance between "zbbz" and "aaaz" is equal to k = 3.
+
+Example 2:
+
+Input: s = "xaxcd", k = 4
+
+Output: "aawcd"
+
+Explanation:
+
+The distance between "xaxcd" and "aawcd" is equal to k = 4.
+
+Example 3:
+
+Input: s = "lol", k = 0
+
+Output: "lol"
+
+Explanation:
+
+It's impossible to change any character as k = 0.
+
+ 
+
+Constraints:
+
+    1 <= s.length <= 100
+    0 <= k <= 2000
+    s consists only of lowercase English letters.
+
+*/
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var getSmallestString = function(s, k) {
+
+    const dist = (a, b) => {
+
+        if(a > b) [a,b] = [b, a];
+
+        return Math.min(b - a, Math.abs(b - a - 26));
+    };
+
+    const a = "a".charCodeAt();
+
+    let ans = "", i = 0;
+
+    while(i < s.length) {
+        
+        const diff = Math.min(dist(a, s.charCodeAt(i)), k);
+
+        ans += String.fromCharCode(Math.min(s.charCodeAt(i) - diff, a + (s.charCodeAt(i) + diff - 26) % a));
+
+        k -= diff;
+        i++;
+    }
+
+    return ans;
+};
