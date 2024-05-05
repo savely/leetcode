e029acfefc5a1cb3d77845b51382b3261b3b9d2d
savely <avelikodny@gmail.com>
2024-05-05 11:25:32 +0200

    #3136. Valid Word - hashmap, accepted

---
+++ validWord.js
@@ -0,0 +1,99 @@
+/*
+#3136. Valid Word
+
+A word is considered valid if:
+
+    It contains a minimum of 3 characters.
+    It consists of the digits 0-9, and the uppercase and lowercase English letters. (Not necessary to have all of them.)
+    It includes at least one vowel.
+    It includes at least one consonant.
+
+You are given a string word.
+
+Return true if word is valid, otherwise, return false.
+
+Notes:
+
+    'a', 'e', 'i', 'o', 'u', and their uppercases are vowels.
+    A consonant is an English letter that is not a vowel.
+
+ 
+
+Example 1:
+
+Input: word = "234Adas"
+
+Output: true
+
+Explanation:
+
+This word satisfies the conditions.
+
+Example 2:
+
+Input: word = "b3"
+
+Output: false
+
+Explanation:
+
+The length of this word is fewer than 3, and does not have a vowel.
+
+Example 3:
+
+Input: word = "a3$e"
+
+Output: false
+
+Explanation:
+
+This word contains a '$' character and does not have a consonant.
+
+ 
+
+Constraints:
+
+    1 <= word.length <= 20
+    word consists of English uppercase and lowercase letters, digits, '@', '#', and '$'.
+
+*/
+
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+var isValid = function(word) {
+
+    if(word.length < 3) return false;
+    
+    const vowels = new Set(['a', 'e', 'i', 'o', 'u']);
+
+    const isDigit = (c) => !Number.isNaN(+c);
+
+    const isEnglishLetter = (c) => {
+        const code = c.toLowerCase().charCodeAt(0), a = 'a'.charCodeAt(), z = 'z'.charCodeAt();
+
+        return  code >= a && code <= z;
+    };
+
+    isVowel = (c) => vowels.has(c.toLowerCase());
+
+    let hasVowel = false; hasConsonant = false;
+
+    for(const c of word) {
+
+        if(isEnglishLetter(c)) {
+            hasVowel ||= isVowel(c);
+            hasConsonant ||= !isVowel(c);
+        } else if(!isDigit(c)) {
+            return false;
+        }
+    }
+
+    return fhasVowel && hasConsonant;
+};
+
+let word = "234Adas";
+word ="Ya$";
+
+console.log(isValid(word));
