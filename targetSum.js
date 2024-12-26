60eb93ae75378574cd1027306d0811ff1bcd8517
savely <avelikodny@gmail.com>
2024-12-26 07:14:26 +0100

    #494. Target Sum - DFS, accepted

--- targetSum.js
+++ targetSum.js
@@ -1,36 +1,69 @@
+/*
+#494. Target Sum
+
+You are given an integer array nums and an integer target.
+
+You want to build an expression out of nums by adding one of the symbols '+' and '-' before each integer in nums and then concatenate all the integers.
+
+    For example, if nums = [2, 1], you can add a '+' before 2 and a '-' before 1 and concatenate them to build the expression "+2-1".
+
+Return the number of different expressions that you can build, which evaluates to target.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,1,1,1], target = 3
+Output: 5
+Explanation: There are 5 ways to assign symbols to make the sum of nums be target 3.
+-1 + 1 + 1 + 1 + 1 = 3
++1 - 1 + 1 + 1 + 1 = 3
++1 + 1 - 1 + 1 + 1 = 3
++1 + 1 + 1 - 1 + 1 = 3
++1 + 1 + 1 + 1 - 1 = 3
+
+Example 2:
+
+Input: nums = [1], target = 1
+Output: 1
+
+ 
+
+Constraints:
+
+    1 <= nums.length <= 20
+    0 <= nums[i] <= 1000
+    0 <= sum(nums[i]) <= 1000
+    -1000 <= target <= 1000
+
+*/
+
+
+
+
 /**
  * @param {number[]} nums
  * @param {number} S
  * @return {number}
  */
- var findTargetSumWays = function(nums, S) {
+var findTargetSumWays = function(nums, S) {
     
-    if(!nums.length) return 0
-    
-    let dp = {}
-    
-    dp[nums[0]] = 1
-    dp[-nums[0]] = 1
-    
-    for(let i = 1; i < nums.length; i++) {
-        
-        const newDp = {}, el = nums[i]
-        
-        for(const sum in dp) {
-            
-            const num = parseInt(sum)
-                  
-            for(const nextSum of [num + el, num - el]) {
-                
-                newDp[nextSum] = (dp[nextSum] || 0) + (newDp[nextSum] || 0) + dp[sum]
-            } 
-        }
-        dp = newDp
-        
-        console.dir(dp)
+    let ans = 0;
+
+    const dfs = (i, sum) => {
+
+        if(i === nums.length) return;
+
+        ans += i === nums.length - 1 && sum + nums[i] === S ? 1 : 0;
+        ans += i === nums.length - 1 && sum - nums[i] === S ? 1 : 0;
+
+        dfs(i + 1, sum + nums[i]);
+        dfs(i + 1, sum - nums[i]);
     }
-    return dp[S] || 0
+
+    dfs(0, 0);
+
+    return ans;
 };
 
-let arr = [1,1,1,1,1]
-console.log(findTargetSumWays(arr,3))
+console.log(findTargetSumWays([1,1,1,1,1], 3)) // 5
