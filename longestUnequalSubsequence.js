df66a53999b3144681a4d4e618127c4b04a7bf52
savely <avelikodny@gmail.com>
2025-05-16 09:44:54 +0200

    #2901. Longest Unequal Adjacent Groups Subsequence II DP, accepted

---
+++ longestUnequalSubsequence.js
@@ -0,0 +1,117 @@
+/*
+#2901. Longest Unequal Adjacent Groups Subsequence II
+
+You are given a string array words, and an array groups, both arrays having length n.
+
+The hamming distance between two strings of equal length is the number of positions at which the corresponding characters are different.
+
+You need to select the longest
+
+from an array of indices [0, 1, ..., n - 1], such that for the subsequence denoted as [i0, i1, ..., ik-1] having length k, the following holds:
+
+    For adjacent indices in the subsequence, their corresponding groups are unequal, i.e., groups[ij] != groups[ij+1], for each j where 0 < j + 1 < k.
+    words[ij] and words[ij+1] are equal in length, and the hamming distance between them is 1, where 0 < j + 1 < k, for all indices in the subsequence.
+
+Return a string array containing the words corresponding to the indices (in order) in the selected subsequence. If there are multiple answers, return any of them.
+
+Note: strings in words may be unequal in length.
+
+ 
+
+Example 1:
+
+Input: words = ["bab","dab","cab"], groups = [1,2,2]
+
+Output: ["bab","cab"]
+
+Explanation: A subsequence that can be selected is [0,2].
+
+    groups[0] != groups[2]
+    words[0].length == words[2].length, and the hamming distance between them is 1.
+
+So, a valid answer is [words[0],words[2]] = ["bab","cab"].
+
+Another subsequence that can be selected is [0,1].
+
+    groups[0] != groups[1]
+    words[0].length == words[1].length, and the hamming distance between them is 1.
+
+So, another valid answer is [words[0],words[1]] = ["bab","dab"].
+
+It can be shown that the length of the longest subsequence of indices that satisfies the conditions is 2.
+
+Example 2:
+
+Input: words = ["a","b","c","d"], groups = [1,2,3,4]
+
+Output: ["a","b","c","d"]
+
+Explanation: We can select the subsequence [0,1,2,3].
+
+It satisfies both conditions.
+
+Hence, the answer is [words[0],words[1],words[2],words[3]] = ["a","b","c","d"].
+
+It has the longest length among all subsequences of indices that satisfy the conditions.
+
+Hence, it is the only answer.
+
+ 
+
+Constraints:
+
+    1 <= n == words.length == groups.length <= 1000
+    1 <= words[i].length <= 10
+    1 <= groups[i] <= n
+    words consists of distinct strings.
+    words[i] consists of lowercase English letters.
+
+*/
+
+/**
+ * @param {string[]} words
+ * @param {number[]} groups
+ * @return {string[]}
+ */
+var getWordsInLongestSubsequence = function(words, groups) {
+
+    const isDistanceOne = (a, b) => {
+
+        if(a.length !== b.length) return false;
+
+        let count = 0;
+        for(let i = 0; i < a.length; i++) {
+            count += a[i] !== b[i] ? 1 : 0;
+            if(count > 1) return false;
+        }
+        return count === 1;
+    };
+
+    const n = words.length, dp = new Array(n).fill(1), prev = new Array(n).fill(-1);
+
+    let maxLength = 1, maxIndex = 0;
+
+    for(let i = 0; i < n; i++) {
+        for(let j = 0; j < i; j++) {
+            if(groups[i] !== groups[j] && isDistanceOne(words[i], words[j])) {
+                if(dp[j] + 1 > dp[i]) {
+                    dp[i] = dp[j] + 1;
+                    prev[i] = j;
+                }
+            }
+
+            if(dp[i] > maxLength) {
+                maxLength = dp[i];
+                maxIndex = i;
+            }
+        }
+    }
+
+    const res = [];
+    while(maxIndex !== -1) {
+        res.push(words[maxIndex]);
+        maxIndex = prev[maxIndex];
+    }
+
+    return res.reverse();
+};
