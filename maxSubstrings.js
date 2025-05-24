410681c5a151577f46adc1f10cb099fdcc38ee82
savely <avelikodny@gmail.com>
2025-05-24 21:39:46 +0200

    #3557. Find Maximum Number of Non Intersecting Substrings - greedy, accepted

---
+++ maxSubstrings.js
@@ -0,0 +1,71 @@
+/*
+#3557. Find Maximum Number of Non Intersecting Substrings
+
+You are given a string word.
+
+Return the maximum number of non-intersecting substrings of word that are at least four characters long and start and end with the same letter.
+
+A substring is a contiguous non-empty sequence of characters within a string.
+
+ 
+
+Example 1:
+
+Input: word = "abcdeafdef"
+
+Output: 2
+
+Explanation:
+
+The two substrings are "abcdea" and "fdef".
+
+Example 2:
+
+Input: word = "bcdaaaab"
+
+Output: 1
+
+Explanation:
+
+The only substring is "aaaa". Note that we cannot also choose "bcdaaaab" since it intersects with the other substring.
+
+ 
+
+Constraints:
+
+    1 <= word.length <= 2 * 105
+    word consists only of lowercase English letters.
+
+*/
+
+/**
+ * @param {string} word
+ * @return {number}
+ */
+var maxSubstrings = function(word) {
+
+    const lastIndex = new Array(26).fill(-1), a = 'a'.charCodeAt(), minDist = 3;
+
+    let lastRight = -1, count = 0;
+
+    for(let i = 0; i < word.length; i++) {
+
+        const ch = word.charCodeAt(i) - a;
+
+        if(lastIndex[ch] < 0 || lastIndex[ch] <= lastRight) {
+            lastIndex[ch] = i;
+            continue;
+        }
+
+        if(i - lastIndex[ch] >= minDist) {
+            count++;
+            lastIndex[ch] = i;
+            lastRight = i;
+        }
+    }
+    return count;
+};
+
+let word = "abcdeafdef";
+
+console.log(maxSubstrings(word));
